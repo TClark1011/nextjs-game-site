6cd2b5bd20954b632c18ea3dd61e1c7b97ce4598
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-01-25 17:18:45 +1100

    tweaked styles

--- components/GameList.tsx
+++ components/GameList.tsx
@@ -1,30 +1,34 @@
-import { Box, Image, Link, SimpleGrid, Text } from "@chakra-ui/react";
+import {
+	Box,
+	ChakraStyleProps,
+	Image,
+	Link,
+	SimpleGrid,
+} from "@chakra-ui/react";
 import IGame from "../types/IGame";
 
-interface Props {
+interface Props extends ChakraStyleProps {
 	games: IGame[];
 }
 
 const GameList: React.FC<Props> = ({ games, ...props }) => {
 	return (
-		<div {...props}>
-			<SimpleGrid columns={[2, 3, 4, 6, 8]}>
-				{games.map(({ name, background_image, id }, index) => (
-					<Box>
-						<Image
-							key={id}
-							src={background_image}
-							w="200px"
-							h="350px"
-							objectFit="cover"
-						/>
-						<Link fontWeight="bold" href={`/games/${id}`}>
-							{name}
-						</Link>
-					</Box>
-				))}
-			</SimpleGrid>
-		</div>
+		<SimpleGrid columns={[2, 3, 4, 6, 8]} {...props}>
+			{games.map(({ name, background_image, id }, index) => (
+				<Box>
+					<Image
+						key={id}
+						src={background_image}
+						w="100%"
+						h="350px"
+						objectFit="cover"
+					/>
+					<Link fontWeight="bold" href={`/games/${id}`}>
+						{name}
+					</Link>
+				</Box>
+			))}
+		</SimpleGrid>
 	);
 };
 
