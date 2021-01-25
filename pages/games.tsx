1f29178a72ea7e7e202294ee19632654656e98a6
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-01-25 17:18:58 +1100

    Added metadata + use Chakra components

--- pages/games.tsx
+++ pages/games.tsx
@@ -1,4 +1,6 @@
+import { Box, Heading } from "@chakra-ui/react";
 import { GetServerSideProps } from "next";
+import Head from "next/head";
 import GameList from "../components/GameList";
 import { fetchGames } from "../services/games";
 import IGame from "../types/IGame";
@@ -7,12 +9,15 @@ interface Props {
 	games: IGame[];
 }
 
-const games: React.FC<Props> = ({ games, ...props }) => {
+const games: React.FC<Props> = ({ games }) => {
 	return (
-		<div>
-			<h2>Games Page</h2>
-			<GameList games={games} />
-		</div>
+		<Box>
+			<Head>
+				<title>Games</title>
+			</Head>
+			<Heading as="h2">Games</Heading>
+			<GameList games={games} w="100%" />
+		</Box>
 	);
 };
 
