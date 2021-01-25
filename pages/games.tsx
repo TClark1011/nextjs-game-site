b2def396a6d6e0c6ca540bebd1e112c0b5530762
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-01-25 20:37:51 +1100

    added page switching buttons

--- pages/games.tsx
+++ pages/games.tsx
@@ -1,16 +1,17 @@
-import { Box, Heading } from "@chakra-ui/react";
+import { Box, Center, Heading, IconButton, Link } from "@chakra-ui/react";
 import { GetServerSideProps } from "next";
 import Head from "next/head";
 import GameList from "../components/GameList";
 import { fetchGames } from "../services/games";
 import IGame from "../types/IGame";
+import { ArrowBackIcon, ArrowForwardIcon } from "@chakra-ui/icons";
 
 interface Props {
 	games: IGame[];
 	page: number;
 }
 
-const games: React.FC<Props> = ({ games, page }) => {
+const games: React.FC<Props> = ({ games, page = 1 }) => {
 	return (
 		<Box>
 			<Head>
@@ -18,13 +19,33 @@ const games: React.FC<Props> = ({ games, page }) => {
 			</Head>
 			<Heading as="h2">Games</Heading>
 			<GameList games={games} w="100%" />
+			<Center>
+				<IconButton
+					aria-label="previous-page"
+					as={Link}
+					href={`/games?page=${page - 1}`}
+					isDisabled={page === 1}
+				>
+					<ArrowBackIcon />
+				</IconButton>
+				<IconButton
+					aria-label="next-page"
+					as={Link}
+					href={`/games?page=${page + 1}`}
+				>
+					<ArrowForwardIcon />
+				</IconButton>
+			</Center>
 		</Box>
 	);
 };
 
 export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+	const page: number = query.page ? parseInt(query.page as string) : 1;
 	const data: any = await fetchGames(query);
-	return { "props": { "games": data.results } };
+	return {
+		"props": { "games": data.results, page },
+	};
 };
 
 export default games;
