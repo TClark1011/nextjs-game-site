860aefb6afdceec406bca72184d64d88e32f71d3
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-01-26 15:36:46 +1100

    overrode default 'sizes' theme property

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,19 +1,22 @@
 import { extendTheme } from "@chakra-ui/react";
 
+const refactorUiValues = [
+	"0.25rem",
+	"0.5rem",
+	"0.75rem",
+	"1rem",
+	"1.5rem",
+	"2rem",
+	"3rem",
+	"4rem",
+	"6rem",
+	"8rem",
+	"12rem",
+	"16rem",
+	"24rem",
+];
+
 export default extendTheme({
-	"space": [
-		"0.25rem",
-		"0.5rem",
-		"0.75rem",
-		"1rem",
-		"1.5rem",
-		"2rem",
-		"3rem",
-		"4rem",
-		"6rem",
-		"8rem",
-		"12rem",
-		"16rem",
-		"24rem",
-	],
+	"space": refactorUiValues,
+	"sizes": refactorUiValues,
 });
