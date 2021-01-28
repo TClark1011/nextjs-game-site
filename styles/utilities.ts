eb927fcd82bd9b7057baea1094592b38885c121c
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-01-28 16:30:18 +1100

    created a 'useLandingPageHeight' to generate height of a landing page that accounts for height of top-bar

---
+++ styles/utilities.ts
@@ -0,0 +1,7 @@
+import { useTheme } from "@chakra-ui/react";
+import { topBarHeight } from "./styleVars";
+
+export const useLandingPageHeight = (): string => {
+	const { space } = useTheme();
+	return `calc(100vh - ${space[topBarHeight - 1]})`;
+};
